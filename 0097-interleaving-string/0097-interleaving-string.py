04ce4a23b9f6b5d39729d36ade9c340e83b8fe27
yogasrii <191876458+Yogasrii@users.noreply.github.com>
2026-08-10 19:27:57 +0530

    Time: 33 ms (24.24%), Space: 12.4 MB (49.96%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isInterleave(self, s1, s2, s3):
+        if len(s1) + len(s2) != len(s3):
+            return False
+
+        m = len(s1)
+        n = len(s2)
+
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+
+        dp[0][0] = True
+
+        # Using only s2
+        for j in range(1, n + 1):
+            dp[0][j] = (
+                dp[0][j - 1] and
+                s2[j - 1] == s3[j - 1]
+            )
+
+        # Using only s1
+        for i in range(1, m + 1):
+            dp[i][0] = (
+                dp[i - 1][0] and
+                s1[i - 1] == s3[i - 1]
+            )
+
+        # Use both s1 and s2
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+
+                from_s1 = (
+                    dp[i - 1][j] and
+                    s1[i - 1] == s3[i + j - 1]
+                )
+
+                from_s2 = (
+                    dp[i][j - 1] and
+                    s2[j - 1] == s3[i + j - 1]
+                )
+
+                dp[i][j] = from_s1 or from_s2
+
+        return dp[m][n]
