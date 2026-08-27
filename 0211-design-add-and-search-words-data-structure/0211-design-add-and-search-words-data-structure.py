7e26a2f1307e6bfabdeda7843dbbfa7aa1d85508
yogasrii <191876458+Yogasrii@users.noreply.github.com>
2026-08-27 19:43:25 +0530

    Time: 2543 ms (83.44%), Space: 117.5 MB (21.44%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,44 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        node = self.root
+
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+
+            node = node.children[char]
+
+        node.is_end = True
+
+    def search(self, word):
+
+        def dfs(index, node):
+            if index == len(word):
+                return node.is_end
+
+            char = word[index]
+
+            if char == '.':
+                # Try every possible character
+                for child in node.children.values():
+                    if dfs(index + 1, child):
+                        return True
+
+                return False
+
+            if char not in node.children:
+                return False
+
+            return dfs(index + 1, node.children[char])
+
+        return dfs(0, self.root)
